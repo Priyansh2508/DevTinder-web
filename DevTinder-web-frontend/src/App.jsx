d68a0b4980c5706f046870230626ef45bb220345
BhavitMishra <bhavitmishra@gmail.com>
2025-07-19 01:36:28 +0530

    app.jsx fixed

--- DevTinder-web-frontend/src/App.jsx
+++ DevTinder-web-frontend/src/App.jsx
@@ -1,32 +1,30 @@
+import Body from "./Components/Body";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./Components/Profile";
 import { Provider } from "react-redux";
 import appStore from "./utils/appStore";
-
-import Body from "./Components/Body";
 import Login from "./Components/Login";
-import Profile from "./Components/Profile";
 import Feed from "./Components/Feed";
 import Connections from "./Components/Connections";
 import Requests from "./Components/Requests";
 
 function App() {
   return (
-    <Provider store={appStore}>
-      <BrowserRouter basename="/">
-        <Routes>
-          {/* Public Route */}
-          <Route path="/" element={<Login />} />
-
-          {/* Protected Routes inside Body Layout */}
-          <Route path="/" element={<Body />}>
-            <Route path="profile" element={<Profile />} />
-            <Route path="feed" element={<Feed />} />
-            <Route path="connections" element={<Connections />} />
-            <Route path="requests" element={<Requests />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <>
+      <Provider store={appStore}>
+        <BrowserRouter basename="/">
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/feed" element={<Feed />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/requests" element={<Requests />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
+    </>
   );
 }
 
